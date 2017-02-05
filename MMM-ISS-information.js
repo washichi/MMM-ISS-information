775b5887a3b72de57fbaf05876c9de411183c4f3
Unknown <washichi@hushmail.com>
2017-02-05 16:28:08 +0100

    implemented "people in space"

--- MMM-ISS-information.js
+++ MMM-ISS-information.js
@@ -8,17 +8,30 @@
  * MIT Licensed.
  */
 
-Module.register("MMM-ISS-current-location",{
+Module.register("MMM-ISS-information",{
 
 	// Default module config.
 	defaults: {
+		iss_now: true,
+		iss_people: true,
+		iss_pass: null,
+		latitude: 52.37,
+		longitude: 4.89,
+		amount: 5,
+		map_style: 'https://api.mapbox.com/styles/v1/washichi/ciynd8xjd00be2skeb3l96ncs/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2FzaGljaGkiLCJhIjoiY2l5bmN5OWZhMDAyeTJxcXFrbTBvM3ljaSJ9.2D2Nkf_YtxbPPiwCsXG0WA',
+		map_height: '150px',
+		map_width: '250px',
+		map_zoom: 0,
+
 		initialLoadDelay: 2500, // 2.5 seconds delay
 		retryDelay: 2500,
 		updateInterval: 60000, // every 60 seconds
-		animationSpeed: 1,
+		animationSpeed: 1000,
 		header: "ISS current location",
 		//opennotify parameters
-		apiBase: "http://api.open-notify.org/iss-now.json?"
+		apiNow: "http://api.open-notify.org/iss-now.json?",
+		apiPeople: "http://api.open-notify.org/astros.json", 
+		apiPass: "http://api.open-notify.org/iss-pass.json?"
 	},
 
 
@@ -38,67 +51,65 @@ Module.register("MMM-ISS-current-location",{
 	start: function() {
 		Log.info("Starting module: " + this.name);
 		this.scheduleUpdate(this.config.initialLoadDelay);
-		this.latitude = 52.37;
-		this.longitude = 4.89;
+		this.ISSlatitude = 0;
+		this.ISSlongitude = 0;
 		this.timestamp = null;
 		this.message = null;
 		this.updateTimer = null;
+		this.number = null;
 	},
 
 
 	// Override dom generator.
 	getDom: function() {
-/*
-		var table = document.createElement("table");
-		table.className = "small";
+	var table = document.createElement("table");
+	table.className = "small";
 
-		var latitudeRow = document.createElement("tr");
-		table.appendChild(latitudeRow);
-		var latitudeCell = document.createElement("td");
-		latitudeCell.className = "latitude";
-		latitudeCell.innerHTML = "latitude: " + String(this.latitude);
-		latitudeRow.appendChild(latitudeCell);
-
-		var longitudeRow = document.createElement("tr");
-		table.appendChild(longitudeRow);
-		var longitudeCell = document.createElement("td");
-		longitudeCell.className = "longitude";
-		longitudeCell.innerHTML = "longitude: " + String(this.longitude);
-		longitudeRow.appendChild(longitudeCell);
-
-		var mapRow = document.createElement("tr");
-		table.appendChild(mapRow);
-		var mapCell = document.createElement("td");
+	if(this.config.iss_now)
+	{
 		var mapid = document.createElement("div");
 		mapid.innerHTML = map;
-		mapCell.appendChild(mapid);
-		mapRow.appendChild(mapCell);
+		mapid.style.height = this.config.map_height;
+		mapid.style.width = this.config.map_width;
+		mapid.style.border = "thin solid #FFFFFF";
 
-*/
-		var mapid = document.createElement("div");
-		mapid.innerHTML = map;
-		mapid.style.height = "200px";
-		mapid.style.width = "400px";
-
-		var map = L.map(mapid, {attributionControl: false, zoomControl:false}).setView([0, 0], 1);
+		var map = L.map(mapid, {attributionControl: false, zoomControl:false}).setView([0, 0], this.config.map_zoom);
 		setTimeout(function() {
-    	map.invalidateSize();
-		}, 1);
+    		map.invalidateSize();
+		}, 100);
 
 		var ISSIcon = L.icon({
-	    iconUrl: this.data.path + "iss4.png",
-	    iconSize: [50, 30],
-	    iconAnchor: [25, 15],
-	    //popupAnchor: [50, 25],
+		    iconUrl: this.data.path + 'iss4.png',
+		    iconSize: [50, 30]
+		    //iconAnchor: [25, 15]
 		});
 		var iss = L.marker([0, 0], {icon: ISSIcon}).addTo(map);
-
-        iss.setLatLng([this.latitude, this.longitude]);
-		map.panTo([this.latitude, this.longitude], animate=true);
-		L.tileLayer('https://api.mapbox.com/styles/v1/washichi/ciynd8xjd00be2skeb3l96ncs/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2FzaGljaGkiLCJhIjoiY2l5bmN5OWZhMDAyeTJxcXFrbTBvM3ljaSJ9.2D2Nkf_YtxbPPiwCsXG0WA', {
-    	maxZoom: 15
+		iss.setLatLng([this.ISSlatitude, this.ISSlongitude]);
+		map.fitWorld();
+		//map.panTo([this.ISSlatitude, this.ISSlongitude], animate=true);
+		L.tileLayer(this.config.map_style, {
+			maxZoom: 15
 		}).addTo(map);
-		return mapid;
+
+		//add to table:
+		var mapRow = document.createElement("tr");
+		var mapCell = document.createElement("td");
+		mapCell.appendChild(mapid);
+		mapRow.appendChild(mapCell);
+		table.appendChild(mapRow);
+	}
+
+	if(this.config.iss_people)
+	{
+		var peopleRow = document.createElement("tr");
+		var textCell = document.createElement("td");
+		Log.error("people request, number = " + String(this.number));
+		textCell.innerHTML = "amount of people in space:      " + this.number;
+		peopleRow.appendChild(textCell);
+		table.appendChild(peopleRow);
+	}
+
+	return table;
 	},
 
 	// Override getHeader method.
@@ -123,18 +134,79 @@ Module.register("MMM-ISS-current-location",{
 	 * Calls processWeather on succesfull response.
 	 */
 	updateISS: function() {
-		var url = this.config.apiBase;
+		if(this.config.iss_map)
+		{
+			this.updateISS_now();
+		}
+		if(this.config.iss_people)
+		{
+			this.updateISS_people();
+		}
+		/*
+		if(this.config.iss_pass)
+		{
+			this.updateISS_pass();
+		}
+		*/
+	},
+
+	updateISS_now: function(){
+		var url = this.config.apiNow;
 		var self = this;
 		var issRequest = new XMLHttpRequest();
 		issRequest.open("GET", url, true);
 		issRequest.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					var resp = JSON.parse(this.response);
+					self.ISSlatitude = resp.iss_position.latitude;
+					self.ISSlongitude = resp.iss_position.longitude;
+					self.timestamp = resp.timestamp;
+					self.message = resp.message;
+				}
+				self.show(self.config.animationSpeed, {lockString:this.identifier});
+				self.updateDom();
+				self.scheduleUpdate(self.config.retryDelay);
+			}
+		};
+	issRequest.send();
+	},
+
+	updateISS_people: function(){
+	var url = this.config.apiPeople;
+	var self = this;
+	var issRequest = new XMLHttpRequest();
+	issRequest.open("GET", url, true);
+	issRequest.onreadystatechange = function() {
 		if (this.readyState === 4) {
 			if (this.status === 200) {
 				var resp = JSON.parse(this.response);
-				self.latitude = resp.iss_position.latitude;
-				self.longitude = resp.iss_position.longitude;
+				self.number = resp.number;
+			}
+			self.show(self.config.animationSpeed, {lockString:this.identifier});
+			self.updateDom();
+			self.scheduleUpdate(self.config.retryDelay);
+		}
+	};
+	issRequest.send();
+	},
+
+	updateISS_pass: function(){
+	var url = this.config.apiPass + "lat=" + this.config.latitude + "&lon=" + this.config.longitude + "&callback=CALLBACK";
+	var self = this;
+	var issRequest = new XMLHttpRequest();
+	issRequest.open("GET", url, true);
+	issRequest.onreadystatechange = function() {
+		Log.error("pass request");
+		if (this.readyState === 4) {
+			if (this.status === 200) {
+				/*
+				var resp = JSON.parse(this.response);
+				self.ISSlatitude = resp.iss_position.latitude;
+				self.ISSlongitude = resp.iss_position.longitude;
 				self.timestamp = resp.timestamp;
 				self.message = resp.message;
+				*/
 			}
 			self.show(self.config.animationSpeed, {lockString:this.identifier});
 			self.updateDom();
--- README.md
+++ README.md
@@ -7,8 +7,17 @@ To use this module, add it to the modules array in the `config/config.js` file:
 ````javascript
 	{
 		module: 'MMM-ISS-information',
-		position: 'bottom_right',	// This can be any of the [regions](https://forum.magicmirror.builders/topic/286/regions)
+		position: 'bottom_right',	// This can be any of the regions
 		header: 'ISS information'
+		 config: {
+            // See 'Configuration options' for more information.
+            iss_map: true, 		//shows a map with realtime ISS location
+            iss_people: true, 	//shows how many people are in space
+            iss_pass: true, 	//true: shows a list of the next time ISS passes above given coordinates
+            // the following variables are required if iss_pass is true:
+           	//latitude: 52.37, 	//latitude of your location
+            //longitude: 4.89,	//longitude of your location
+        }
 	}
 ````
 ## Installation
@@ -20,14 +29,16 @@ based on the OpenNotify documentation: [http://open-notify.org/Open-Notify-API]
 
 | Option            | Description
 | ----------------- | -----------
-| `iss-map`         | **REQUIRED** <br><br> Shows an map with the realtime ISS position. <br> **Default value:** `true`
+| `iss_map`         | **REQUIRED** <br><br> Shows an map with the realtime ISS position. <br> **Default value:** `true`
 | `iss_people`      | **REQUIRED** <br><br> Shows the number of people in space. <br> **Default value:** `true`
 | `iss_pass`        | **REQUIRED** <br><br> Lists upcoming ISS passes above given coördinates. <br><br> **Default value:** `true`
 | `latitude`        | **OPTIONAL** <br><br> Latitude, used to display iss_pass. <br> **Default value:** `Amsterdam latitude`
 | `longitude`       | **OPTIONAL** <br><br> Latitude, used to display iss_pass. <br> **Default value:** `Amsterdam longitude`
 | `amount`          | **OPTIONAL** <br><br>amount of ISS passes that are listed. <br> **Default value:** `5`
-| `map_style`       | **OPTIONAL** <br><br>map style. <br> You can create your own style at [mapbox](https://www.mapbox.com/studio/styles) <br> **Default map style:** [preview](https://api.mapbox.com/styles/v1/washichi/ciynd54zw00002spiqbgeyi7y.html?title=true&access_token=pk.eyJ1Ijoid2FzaGljaGkiLCJhIjoiY2l5bmN5OWZhMDAyeTJxcXFrbTBvM3ljaSJ9.2D2Nkf_YtxbPPiwCsXG0WA#1.6/29.811777/41.858478/0)
-| `map_height`      | **OPTIONAL** <br><br>Height of the map. <br> **Default value:** `200px`
-| `map_width`       | **OPTIONAL** <br><br>Width of the map. <br> **Default value:** `400px`
+| `map_style`       | **OPTIONAL** <br><br>map style. <br> You can download/create your own style at [mapbox](https://www.mapbox.com/studio/styles) <br> **Default map style:** [preview](https://api.mapbox.com/styles/v1/washichi/ciynd54zw00002spiqbgeyi7y.html?title=true&access_token=pk.eyJ1Ijoid2FzaGljaGkiLCJhIjoiY2l5bmN5OWZhMDAyeTJxcXFrbTBvM3ljaSJ9.2D2Nkf_YtxbPPiwCsXG0WA#1.6/29.811777/41.858478/0) 
+| `map_height`      | **OPTIONAL** <br><br>Height of the map. <br> **Default value:** `150px`
+| `map_width`       | **OPTIONAL** <br><br>Width of the map. <br> **Default value:** `250px`
 | `map_zoom`        | **OPTIONAL** <br><br>Zoom level of the map. <br> **Default value:** `1`
+
+Many other configuration options for the map are listed in the official Leaflet [documentation](http://leafletjs.com/reference.html).
 
